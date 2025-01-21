f06af9b588dbc0d5a696a1c7e95641b2699b3213
Alireza <ghavami.alireza98@gmail.com>
2025-01-21 14:22:37 +0100

    new UI branch created

--- chat_with_documents.py
+++ chat_with_documents.py
@@ -335,3 +335,11 @@ if choice == 'Vector RAG':
                         os.makedirs("results")
                     results_df.to_excel(results_file, index=False)
                     st.success(f"Results exported successfully to {results_file}!")
+                if os.path.exists("results/results_file.xlsx"):
+                    with open("results/results_file.xlsx", "rb") as file:
+                        st.download_button(
+                            label="Download the results file",
+                            data=file,
+                            file_name="results_file.xlsx",
+                            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                        )
